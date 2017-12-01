0b30d2292679ba5e774976f20c3abdd9dd0743a6
Jose Luis Jardon <jljardon.c@gmail.com>
2017-12-01 05:49:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,6 +22,10 @@ function sayHiToGrandma(string){
 
   if(string === string.toLowerCase()){
     return "I can\'t hear you!"
+  } else if(string === string.toUpperCase(){
+    return "YES INDEED!"
+  }else {
+    return "I love you, too."
   }
 
 }
